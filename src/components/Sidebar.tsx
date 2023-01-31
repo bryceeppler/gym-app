f11c2023a80934041776d3c3dbfd4cd72c8011a0
Bryce Eppler <eppler97@gmail.com>
2023-01-30 18:25:00 -0800

    bugfixes'

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -11,45 +11,8 @@ import { useWorkoutModalStore } from '../store';
 
 export default function Sidebar({user, cardioSessions, icePlunges, completedWorkouts}: Props) {
   // export default function Sidebar() {
-    console.log(user)
-    const { userId, setShowActivityModal } = useWorkoutModalStore();
-    const createIcePlunge = api.users.createIcePlunge.useMutation(
-      {
-        onSuccess: () => {
-          // invalidate query to update the UI
-          // utils.workouts.getIncompleteWorkouts.invalidate()
-          // .catch((err) => console.log(err));  
-        },
-      }
-    );
+    const { setShowActivityModal } = useWorkoutModalStore();
 
-    const [joinedHistory, setJoinedHistory] = useState<any[]>([]);
-    useEffect(() => {
-      if (completedWorkouts && icePlunges && cardioSessions) {
-        let temp = [...completedWorkouts, ...icePlunges, ...cardioSessions];
-        let history = []
-        // array of tuples with (activity, date) where activity is a workout, iceplunge or cardioSession
-        for (let i = 0; i < temp.length; i++) {
-          let activity = temp[i];
-          let date = activity?.date;
-          if (date) {
-            history.push([activity, date]);
-          }
-        }
-        // sort by date. if date is undefined, put it at the end
-        history.sort((a, b) => {
-          if (a[1] && b[1]) {
-            return a[1] > b[1] ? -1 : 1;
-          } else if (a[1]) {
-            return -1;
-          } else {
-            return 1;
-          }
-        })
-        setJoinedHistory(history);
-        console.log("joined history", history);
-      }
-    }, [completedWorkouts, icePlunges, cardioSessions])
 
   return (
     <div className="flex flex-col gap-4">
@@ -78,7 +41,7 @@ export default function Sidebar({user, cardioSessions, icePlunges, completedWork
         {/* map out cardiosessions, ice plunges and recently completed workouts */}
         {completedWorkouts?.map((workout) => {
           return (
-            <div className="flex flex-row justify-between p-2 w-60 mx-auto mb-1 bg-baselight rounded">
+            <div key={workout.id} className="flex flex-row justify-between p-2 w-60 mx-auto mb-1 bg-baselight rounded">
               <div className="text-sm font-bold">{workout.title}</div>
               <div className="text-sm font-bold">{workout.date?.toLocaleDateString()}</div>
             </div>
