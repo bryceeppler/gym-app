5ced3c9bdfe0d26ae7513c4fe30142bd47e8994d
Bryce Eppler <eppler97@gmail.com>
2023-01-30 18:18:13 -0800

    llokin good

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { api } from "../utils/api";
-import { users, cardioSession, icePlunge} from '@prisma/client';
+import { users, cardioSession, icePlunge, completedWorkouts} from '@prisma/client';
 type Props = {
   user?: users;
   cardioSessions?: cardioSession[];
   icePlunges?: icePlunge[];
+  completedWorkouts?: completedWorkouts[];
 }
 import { useWorkoutModalStore } from '../store';
 
-export default function Sidebar({user, cardioSessions, icePlunges}: Props) {
+export default function Sidebar({user, cardioSessions, icePlunges, completedWorkouts}: Props) {
   // export default function Sidebar() {
     console.log(user)
     const { userId, setShowActivityModal } = useWorkoutModalStore();
@@ -21,6 +22,35 @@ export default function Sidebar({user, cardioSessions, icePlunges}: Props) {
         },
       }
     );
+
+    const [joinedHistory, setJoinedHistory] = useState<any[]>([]);
+    useEffect(() => {
+      if (completedWorkouts && icePlunges && cardioSessions) {
+        let temp = [...completedWorkouts, ...icePlunges, ...cardioSessions];
+        let history = []
+        // array of tuples with (activity, date) where activity is a workout, iceplunge or cardioSession
+        for (let i = 0; i < temp.length; i++) {
+          let activity = temp[i];
+          let date = activity?.date;
+          if (date) {
+            history.push([activity, date]);
+          }
+        }
+        // sort by date. if date is undefined, put it at the end
+        history.sort((a, b) => {
+          if (a[1] && b[1]) {
+            return a[1] > b[1] ? -1 : 1;
+          } else if (a[1]) {
+            return -1;
+          } else {
+            return 1;
+          }
+        })
+        setJoinedHistory(history);
+        console.log("joined history", history);
+      }
+    }, [completedWorkouts, icePlunges, cardioSessions])
+
   return (
     <div className="flex flex-col gap-4">
     <div className="text-white text-lg font-bold">{user?.username}</div>
@@ -30,7 +60,7 @@ export default function Sidebar({user, cardioSessions, icePlunges}: Props) {
         <div className="text-white text-2xl font-bold">{
           // sum all the "duration" fields in icePlunges
           icePlunges?.reduce((acc, curr) => acc + (curr.duration || 0), 0)
-          
+
         }</div>
       </div>
       <div className="flex flex-col">
@@ -40,6 +70,22 @@ export default function Sidebar({user, cardioSessions, icePlunges}: Props) {
           cardioSessions?.reduce((acc, curr) => acc + (curr.duration || 0), 0)
         }</div>
       </div>
+      </div>
+      <div className="flex flex-col w-full text-white text-left">
+        <div className="font-bold text-lg mb-4">
+          Recently completed
+        </div>
+        {/* map out cardiosessions, ice plunges and recently completed workouts */}
+        {completedWorkouts?.map((workout) => {
+          return (
+            <div className="flex flex-row justify-between p-2 w-60 mx-auto mb-1 bg-baselight rounded">
+              <div className="text-sm font-bold">{workout.title}</div>
+              <div className="text-sm font-bold">{workout.date?.toLocaleDateString()}</div>
+            </div>
+          )
+        })}
+          
+        
       </div>
     <button className="transition-colors bg-coldblue hover:bg-opacity-60 w-full text-white py-4 font-bold text-sm rounded"
     onClick={() => {
--- src/pages/dashboard/[uid].tsx
+++ src/pages/dashboard/[uid].tsx
@@ -14,28 +14,28 @@ type Props = {
 };
 
 export default function UserDashboard({ uid }: Props) {
+  const { showWorkoutModal, setUserId, showActivityModal, setCurrentUser } =
+    useWorkoutModalStore();
+  // fetch all user data with completed workouts
+  const { data: userList } = api.users.getUserList.useQuery();
 
-    const { showWorkoutModal, setUserId, showActivityModal, setCurrentUser } = useWorkoutModalStore();
-    // fetch all user data with completed workouts
-    const { data:userList} = api.users.getUserList.useQuery()
+  const { data: workoutList } = api.workouts.getWorkoutList.useQuery();
 
-    const { data:workoutList } = api.workouts.getWorkoutList.useQuery();
+  const { data: incompleteWorkouts, isLoading } =
+    api.workouts.getIncompleteWorkouts.useQuery({ id: uid });
 
-    const { data:incompleteWorkouts, isLoading } = api.workouts.getIncompleteWorkouts.useQuery({id:uid})
-
-    useEffect(() => {
-      // userList && userList.find((user) => user.id === uid) ? () => {
-      //   setUserId(uid) && setCurrentUser(userList.find((user) => user.id === uid));
-      // } : () => {
-      //   console.log("ppo"")
-      if (userList) {
-        if (userList.find((user) => user.id === uid)) {
-          setUserId(uid) 
-          // setCurrentUser(userList.find((user) => user.id === uid));
-        }
+  useEffect(() => {
+    // userList && userList.find((user) => user.id === uid) ? () => {
+    //   setUserId(uid) && setCurrentUser(userList.find((user) => user.id === uid));
+    // } : () => {
+    //   console.log("ppo"")
+    if (userList) {
+      if (userList.find((user) => user.id === uid)) {
+        setUserId(uid);
+        // setCurrentUser(userList.find((user) => user.id === uid));
       }
-        
-    }, [userList]);
+    }
+  }, [userList]);
 
   return (
     <>
@@ -47,13 +47,13 @@ export default function UserDashboard({ uid }: Props) {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-row justify-center bg-base p-4 gap-4">
+      <main className="flex min-h-screen flex-row justify-center gap-4 bg-base p-4">
         {showWorkoutModal && <WorkoutModal />}
         {showActivityModal && <ActivityModal />}
         {/* navbar placeholder */}
         <div className="hidden h-full w-16 flex-col items-center justify-center lg:flex">
           {/* <img src="/favicon.ico" className="h-12 w-12 rounded bg-paper" /> */}
-          <div className="h-12 w-12 rounded bg-paper transition-colors hover:bg-baselight flex justify-center items-center">
+          <div className="flex h-12 w-12 items-center justify-center rounded bg-paper transition-colors hover:bg-baselight">
             <img src="/favicon.ico" className="h-4 w-4" />
           </div>
         </div>
@@ -61,31 +61,66 @@ export default function UserDashboard({ uid }: Props) {
         <div className=" min-h-screen w-full rounded bg-baselight p-6">
           <div className="grid grid-cols-12 gap-4">
             {/* main section */}
-            <div className="col-span-12 min-h-screen lg:col-span-8 space-y-8">
-              <UpcomingWorkouts 
-                workouts={incompleteWorkouts}
+            <div className="col-span-12 min-h-screen space-y-8 lg:col-span-8">
+              <UpcomingWorkouts workouts={incompleteWorkouts} />
+              <Stats
+                icePlunges={
+                  userList?.find((user) => user.id === uid)?.icePlunges.length
+                }
+                cardioSessions={
+                  userList?.find((user) => user.id === uid)?.cardioSessions
+                    .length
+                }
+                workouts={
+                  userList?.find((user) => user.id === uid)?.completedWorkouts
+                    .length
+                }
+                skipped={
+                  userList
+                    ?.find((user) => user.id === uid)
+                    ?.completedWorkouts.filter(
+                      (workout) => workout.status === "skipped"
+                    ).length
+                }
               />
-              <Stats 
-                icePlunges={userList?.find((user) => user.id === uid)?.icePlunges.length}
-                cardioSessions={userList?.find((user) => user.id === uid)?.cardioSessions.length}
-                workouts={userList?.find((user) => user.id ===uid)?.completedWorkouts.length}
-                skipped={userList?.find((user) => user.id === uid)?.completedWorkouts.filter((workout) => workout.status === "skipped").length}
-              />
-              <Progress 
+              <Progress
                 workouts={workoutList}
                 users={userList}
-                completedWorkouts1={userList?.find((user) => user.id === 1)?.completedWorkouts}
-                completedWorkouts2={userList?.find((user) => user.id === 2)?.completedWorkouts}
-                icePlunges1={userList?.find((user) => user.id === 1)?.icePlunges}
-                icePlunges2={userList?.find((user) => user.id === 2)?.icePlunges}
-                cardioSessions1={userList?.find((user) => user.id === 1)?.cardioSessions}
-                cardioSessions2={userList?.find((user) => user.id === 2)?.cardioSessions}
+                completedWorkouts1={
+                  userList?.find((user) => user.id === 1)?.completedWorkouts
+                }
+                completedWorkouts2={
+                  userList?.find((user) => user.id === 2)?.completedWorkouts
+                }
+                icePlunges1={
+                  userList?.find((user) => user.id === 1)?.icePlunges
+                }
+                icePlunges2={
+                  userList?.find((user) => user.id === 2)?.icePlunges
+                }
+                cardioSessions1={
+                  userList?.find((user) => user.id === 1)?.cardioSessions
+                }
+                cardioSessions2={
+                  userList?.find((user) => user.id === 2)?.cardioSessions
+                }
               />
             </div>
 
             {/* right sidebar */}
-            <div className="col-span-12 lg:min-h-screen bg-paper lg:col-span-4 p-8">
-              <Sidebar user={userList?.find((user) => user.id === uid)} cardioSessions={userList?.find((user) => user.id === uid)?.cardioSessions} icePlunges={userList?.find((user) => user.id === uid)?.icePlunges}/>
+            <div className="col-span-12 bg-paper p-8 lg:col-span-4 lg:min-h-screen">
+              <Sidebar
+                user={userList?.find((user) => user.id === uid)}
+                cardioSessions={
+                  userList?.find((user) => user.id === uid)?.cardioSessions
+                }
+                icePlunges={
+                  userList?.find((user) => user.id === uid)?.icePlunges
+                }
+                completedWorkouts={
+                  userList?.find((user) => user.id === uid)?.completedWorkouts
+                }
+              />
             </div>
           </div>
         </div>
@@ -94,9 +129,9 @@ export default function UserDashboard({ uid }: Props) {
   );
 }
 
-export function getServerSideProps(context: { params: { uid: number } }) { 
-    // uid is coming as a string, need to convert to number
-    const uid = Number(context.params.uid);
+export function getServerSideProps(context: { params: { uid: number } }) {
+  // uid is coming as a string, need to convert to number
+  const uid = Number(context.params.uid);
   return {
     props: {
       uid: uid,
